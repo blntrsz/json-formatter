f1e8d223c2a3e043e3d19a431ea251128f2d9690
Balint Orosz <orosz.balint.97@gmail.com>
2022-11-02 21:54:27 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState, ChangeEventHandler } from "react";
+import ReactJson from "react-json-view";
+
+function App() {
+  const [json, setJson] = useState({});
+  const [error, setError] = useState<string>("");
+
+  const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    try {
+      setJson(JSON.parse(e.target.value));
+      setError("");
+    } catch (e) {
+      setError(e.message as string);
+    }
+  };
+
+  return (
+    <main>
+      <textarea onChange={onChange} />
+      <div className={`validator${error ? " error" : " valid"}`}>
+        <Validator error={error} />
+      </div>
+      <ReactJson theme="monokai" src={json} />
+    </main>
+  );
+}
+
+const Validator = ({ error }: { error?: string }) => {
+  if (error) {
+    return (
+      <details>
+        <summary>It has error!</summary>
+        <span>{JSON.stringify(error)}</span>
+      </details>
+    );
+  }
+
+  return <div>No error</div>;
+};
+
+export default App;
